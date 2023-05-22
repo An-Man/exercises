2f8ed2abb21f2da0fc473262247963cd0981a91b
An-Man <anna.pikkarainen@gmail.com>
2023-05-22 12:39:27 +0300

    update

--- exercise2.1/exercise2.1.cpp
+++ exercise2.1/exercise2.1.cpp
@@ -5,7 +5,28 @@
 
 int main()
 {
-    std::cout << "Dieselin hinta on " << constants::dieselEuroa << " euroa.\n";
+    //kysy matkan pituus
+    std::cout << "Anna ajettu matka (km): ";
+    double matka {};
+    std::cin >> matka;
+
+    //kysy kulutus satasella
+    std::cout << "Anna polttoaineen kulutus l/100 km: ";
+    double kulutus {};
+    std::cin >> kulutus;
+
+    //kysy diesel vai bensa
+    std::cout << "Diesel vai bensa [D/B]? ";
+    char polttisTyyppi {};
+    std::cin >> polttisTyyppi;
+    
+    //laske kustannus
+    if (polttisTyyppi == 'D')
+        std::cout << "Polttoainekustannus on " << ((matka * kulutus) / 100) 
+        * constants::dieselEuroa << " euroa.\n";
+    else
+        std::cout << "Polttoainekustannus on " << ((matka * kulutus) / 100) 
+        * constants::e95Euroa << " euroa.\n";
 
     return 0;
 }
