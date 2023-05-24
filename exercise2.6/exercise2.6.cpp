fddd9b9a67ce3b29ee84ef328286364051fc1fcd
An-Man <anna.pikkarainen@gmail.com>
2023-05-24 15:01:04 +0300

    uusi exercise 2.6, kesken

---
+++ exercise2.6/exercise2.6.cpp
@@ -0,0 +1,19 @@
+//exercise 2.6 Palkkatehtävä
+
+#include <iostream>
+
+int main()
+{
+    int paiva {0};
+
+    for (int palkka{1}; palkka <= 10'000'000;palkka *= 2)
+        {
+        ++paiva;
+        std::cout << "päivä" << paiva << "palkka" << palkka << "euroa\n";
+        }
+        
+
+    std::cout << paiva;
+
+    return 0;
+}
