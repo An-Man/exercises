a576e8b9344061ac73eb52fff893028782007a6f
An-Man <anna.pikkarainen@gmail.com>
2023-05-25 12:01:07 +0300

    valmis exercise2.6

--- exercise2/exercise2.6/exercise2.6.cpp
+++ exercise2/exercise2.6/exercise2.6.cpp
@@ -2,18 +2,22 @@
 
 #include <iostream>
 
+//funktio kertoo päivän, palkan ja palkkojen summan kun palkka tuplaantuu joka päivä
+//luuppi pyörii niin kauan kuin palkkojen summa on alle tavoitteen
+void palkkalaskuri(int tavoite)
+{
+    int palkkasumma {};
+    for (int palkka{1}, paiva{1}; palkkasumma < tavoite; palkka *= 2, ++paiva)
+        {
+            palkkasumma += palkka;
+            std::cout << "päivä: " << paiva << " palkka: " << palkka << 
+                " palkkasumma: " << palkkasumma << " euroa\n";
+        } 
+}
+
 int main()
 {
-    int paiva {0};
-
-    for (int palkka{1}; palkka < 10'000'000;palkka *= 2)
-        {
-        ++paiva;
-        std::cout << "päivä" << paiva << "palkka" << palkka << "euroa\n";
-        }
-        
-
-    std::cout << paiva;
+    palkkalaskuri(10'000'000);
 
     return 0;
 }
