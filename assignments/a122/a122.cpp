89c8f5780baee918a39c3acedfa792d3fc413f1a
An-Man <anna.pikkarainen@gmail.com>
2023-06-06 15:42:12 +0300

    add modified file a122.cpp

--- assignments/a122/a122.cpp
+++ assignments/a122/a122.cpp
@@ -2,3 +2,74 @@
 #include <iostream>
 #include <string>
 
+std::string get_password()
+{
+    std::cout << "Enter a strong password: ";
+    std::string passwd;
+    std::cin >> passwd;
+
+    return passwd;
+}
+
+bool has_lower(std::string str)
+{
+    int length = str.length();
+    bool lower {false};
+
+    for (int i = 0; i < length; ++i)
+        if (std::islower(str[i]))      
+            lower = true;
+        
+    return lower;
+}
+
+bool has_upper(std::string str)
+{
+    int length = str.length();
+    bool upper {false};
+
+    for (int i = 0; i < length; ++i)
+        if (std::isupper(str[i]))      
+            upper = true;
+        
+    return upper;
+}
+
+bool has_digit(std::string str)
+{
+    int length = str.length();
+    bool digit {false};
+
+    for (int i = 0; i < length; ++i)
+        if (std::isdigit(str[i]))      
+            digit = true;
+        
+    return digit;
+}
+
+bool has_spec_ch(std::string str)
+{
+    int length = str.length();
+    bool spec_ch {false};
+
+    for (int i = 0; i < length; ++i)
+        if (!std::isalnum(str[i]))      
+            spec_ch = true;
+        
+    return spec_ch;
+}
+
+int main()
+{
+    std::string password {get_password()};
+    int length = password.length();
+
+    if (has_lower && has_upper && has_digit &&
+        has_spec_ch && (length >= 8))
+        std::cout << "Password is strong.\n";
+    
+    else
+        std::cout << "Password is not strong.\n";
+
+    return 0;
+}
