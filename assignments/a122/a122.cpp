5342d531629cd5d52ec070e5bab014af75e4ff52
An-Man <anna.pikkarainen@gmail.com>
2023-06-06 20:39:27 +0300

    modify function to check passwd

--- assignments/a122/a122.cpp
+++ assignments/a122/a122.cpp
@@ -11,65 +11,53 @@ std::string get_password()
     return passwd;
 }
 
-bool has_lower(std::string str)
+// function returns true if password is strong - string given as reference
+bool is_strong_password(const std::string& password)
 {
-    int length = str.length();
-    bool lower {false};
+    //criteria for strong password
+    const int min_length {8};
+    const int min_uppercase {1};
+    const int min_lowercase {1};
+    const int min_digits {1};
+    const int min_special_chars {1};
 
-    for (int i = 0; i < length; ++i)
-        if (std::islower(str[i]))      
-            lower = true;
-        
-    return lower;
-}
+    //discard short password
+    if (password.length() < min_length)
+    {
+        return false;
+    }
 
-bool has_upper(std::string str)
-{
-    int length = str.length();
-    bool upper {false};
+    bool has_uppercase {false};
+    bool has_lowercase {false};
+    bool has_digit {false};
+    bool has_special_char {false};
 
-    for (int i = 0; i < length; ++i)
-        if (std::isupper(str[i]))      
-            upper = true;
-        
-    return upper;
-}
-
-bool has_digit(std::string str)
-{
-    int length = str.length();
-    bool digit {false};
-
-    for (int i = 0; i < length; ++i)
-        if (std::isdigit(str[i]))      
-            digit = true;
-        
-    return digit;
-}
-
-bool has_spec_ch(std::string str)
-{
-    int length = str.length();
-    bool spec_ch {false};
-
-    for (int i = 0; i < length; ++i)
-        if (!std::isalnum(str[i]))      
-            spec_ch = true;
-        
-    return spec_ch;
+    //check inside string range with cctype functions
+    for (char c : password)
+    {
+        if (std::isupper(c)) {
+            has_uppercase = true;
+        } else if (std::islower(c)) {
+            has_lowercase = true;
+        } else if (std::isdigit(c)) {
+            has_digit = true;
+        } else if (!std::isalnum(c)) {
+            has_special_char = true;
+        }
+    }
+    
+    return (has_uppercase && has_lowercase && has_digit && has_special_char);
 }
 
 int main()
 {
     std::string password {get_password()};
-    int length = password.length();
-
-    if (has_lower && has_upper && has_digit &&
-        has_spec_ch && (length >= 8))
-        std::cout << "Password is strong.\n";
     
-    else
+    if (is_strong_password(password)) {
+        std::cout << "Password is strong.\n";
+    } else {
         std::cout << "Password is not strong.\n";
+    }
 
     return 0;
 }
