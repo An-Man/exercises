8b3eefe9d313f000f70daf447b2c6087377bab46
An-Man <anna.pikkarainen@gmail.com>
2023-06-08 20:58:42 +0300

    new a132_ex_2_

--- assignments/a132/a132_2/a132_ex_2_main.cpp
+++ assignments/a132/a132_2/a132_ex_2_main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <map> // not implemented yet
-#include <string>
+#include <map>
 
-// A banking program -- Assignment 1_3_2, Exercise 1
+// A banking program -- Assignment 1_3_2, Exercise 2
 
-struct Account
+struct Account // variables of account
 {
     int acc_num {};
     int acc_balance {};
@@ -16,8 +15,7 @@ struct User  // all variables related to user
 	std::string address {};
 	std::string tel {};
 	int customer_num {};
-	long balance {0};
-    std::map<int, Account> accounts; // not implemented yet
+    std::map<int, Account> accounts;
 
 };
 
@@ -29,53 +27,75 @@ void enter_user_info(User& user)  // gets data from user
     std::cout << "Enter address: ";
     std::getline(std::cin, user.address);
 
-    std::cout << "Enter telephone number : ";
+    std::cout << "Enter telephone number: ";
     std::getline(std::cin, user.tel);
 
     std::cout << "Enter your customer nr: ";
     std::cin >> user.customer_num;
 }
 
-void check_balance(User& user)
+void check_balance(User& user) // checks balance on account, asks which number
 {
     std::cout << "Let's check the balance." << '\n';
-    std::cout << "Your account balance is: " << user.balance << '\n';
+    std::cout << "Enter account number (default account is 1001): ";
+    int input {};
+    std::cin >> input;
+
+    std::cout << "Balance on account nr "
+        << input << " is: " << user.accounts[input].acc_balance << '\n';
 }
 
-void add_money(User& user)
+void add_money(User& user) // add money on chosen account
 {
     std::cout << "Let's add money" << '\n';
+    std::cout << "Enter account number (default account is 1001): ";
+    int input {};
+    std::cin >> input;
     std::cout << "Input sum to add to account: ";
     int add {};
     std::cin >> add;
-    user.balance += add;
+    user.accounts[input].acc_balance += add;
     std::cout << add << " added to account balance." << '\n';                        
 }            
 
-void withdraw_money(User& user)
+void withdraw_money(User& user) // withdraw from account
 {
     std::cout << "Let's withdraw money" << '\n';
-    std::cout << "Input withdraw money from account: " << '\n';
+    std::cout << "Enter account number (default account is 1001): ";
+    int input {};
+    std::cin >> input;
+    std::cout << "Input sum to withdraw money from account: " << '\n';
     int withdr {};
     std::cin >> withdr;
-    user.balance -= withdr;
+    user.accounts[input].acc_balance -= withdr;
     std::cout << withdr << " taken from account balance." << '\n';                                
-    }  
+    }
+
+void add_new_account(User& user) // lets the user create another account
+{
+    std::cout << "Let's add another bank account" << '\n';
+    std::cout << "Input unique account number: " << '\n';
+    int input {};
+    std::cin >> input;
+
+    Account new_account {input, 0};     // new instance of Account, acc_number is the user input
+    user.accounts[input] = new_account; // connects user to the account by same number
+}
 
 int main()
 {
-    User user1; // begin with user1
+    User user1 {}; // creates first user
 
-    Account acc01; //create initial account
-
-    enter_user_info(user1); // ask user to input data
-
-    user1.accounts[user1.customer_num]  = acc01; // pair customer num with account ?!?!?
+    Account acc01 = {1001, 9000};
+    
+    user1.accounts[1001] = acc01; // account created and linked to user1 in map
+     
+    enter_user_info(user1); // user info is input at start
 
     while (true) // loop gives user 4 choices 
     {
         std::cout << "Press (1) to check balance, (2) to add money, (3) "
-         "to withdraw or (4) to quit: ";
+         "to withdraw, (4) to add new account, (5) to quit: ";
         int choice {};
         std::cin >> choice;
 
@@ -97,6 +117,11 @@ int main()
                 break;
             }
             case 4:
+            {
+                add_new_account(user1);
+                break;
+            }
+            case 5:
                 std::cout << "Bye bye!" << '\n'; // quits program
                 return 0;
             default:
