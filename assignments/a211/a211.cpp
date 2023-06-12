290e27c9091235d2384a08d4be18e0d3a2c948de
An-Man <anna.pikkarainen@gmail.com>
2023-06-12 09:49:11 +0300

    filename ch to a121 and new a211

---
+++ assignments/a211/a211.cpp
@@ -0,0 +1,15 @@
+/* Exercise 1:
+
+Create an array with 10 random numbers in it. 
+Your program should also be able to output the elements in the array. */
+
+#include <iostream>
+#include <array>
+
+int main()
+{
+    std::array<int, 10> ar {2, 4, 19, 3, 1, 7, 8, 5, 6, 11};
+
+    for(const auto& num : ar)
+        
+}
