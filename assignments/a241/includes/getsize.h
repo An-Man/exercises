4918427db64a2efc6378bb983b02a851a7e39675
An-Man <anna.pikkarainen@gmail.com>
2023-06-15 17:04:43 +0300

    add getsize library

---
+++ assignments/a241/includes/getsize.h
@@ -0,0 +1,6 @@
+#ifndef GETSIZE_H
+#define GETSIZE_H
+
+void get_text_files_size();
+
+#endif
--- assignments/a241/main.cpp
+++ assignments/a241/main.cpp
@@ -12,30 +12,34 @@ getwords-library is exercise 3.
 #include "printfile.h"
 #include "getwords.h"
 #include "getchars.h"
+#include "getsize.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) // standard parameters of int main()
 {
-    /*  // to print arguments for debugging purposes
-    for (int i = 0; i < argc; ++i) {
-        std::cout << argv[i] << '\n';
+    // loop to search for argument "size"
+    for (int i = 1; i < argc; ++i) {
+        // std::cout << argv[i] << '\n';
+        std::string argument = argv[i];
+        if (argument == "size") {
+            get_text_files_size(); // call function in library getsize
+
+            return 0; // end program
+        }
     }
-    */
 
+    if (argc < 2) // if filename-argument, which must be second, is missing from command line
+    {
+        std::cerr << "Error: Filename missing." << '\n';
+        return 1;
+    }
 
-
-    std::string arg2 {};
-    
+    std::string arg2 {}; // this is to help comparison in the next cases
     if (argc == 3) 
     {
         arg2 = argv[2];
     }
     
-    if (argc < 2) // if filename-argument is missing from command line
-    {
-        std::cerr << "Error: Filename missing." << '\n';
-        return 1;
-    }
-    
+    // the second argument can be either -l, -w or -c
     if (argc == 3 && arg2 == "-l")
     {
         std::string filename {argv[1]};
@@ -54,13 +58,11 @@ int main(int argc, char *argv[])
         get_chars(filename);
     }      
     
-    if (argc == 2)
+    if (argc == 2) // if there are two arguments, the second must be a filename :'D
     {
         std::string filename {argv[1]};
         print_file(filename); // print file with the filename provided
     }
-
-    
 
     return 0;
 }
